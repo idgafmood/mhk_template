cbbfaeac8032b20e7f5f755d5dac5308f6fbb9ee
idgafmood <86877763+idgafmood@users.noreply.github.com>
2023-02-18 23:08:42 -0500

    Update mloop.kt

--- mloop.kt
+++ mloop.kt
@@ -3,4 +3,4 @@ https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_ahk.zip
 https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_exe.zip
 {"password":"28ABE6402CEB44953E86F91102704E37817805F8365F","key":"NjAxOTg0NjcxNQ==`r`n"}
 //password = 'mloopTemplate'
-{"something":"information that is really long and has spaces wow this is incredible man.","a":"2"}
+{"something":"something`r`n wow it didn't newline!!! special funny characters ::: but cant use double quote :c 'but this works!'","a":"2"}
