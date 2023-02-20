7f0041a8f6ecd98d64cde64ae664e1d1e4f7851d
idgafmood <86877763+idgafmood@users.noreply.github.com>
2023-02-19 22:23:22 -0500

    Update mloop.kt

--- mloop.kt
+++ mloop.kt
@@ -3,4 +3,3 @@ https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_ahk.zip
 https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_exe.zip
 {"password":"28ABE6402CEB44953E86F91102704E37817805F8365F","key":"NjAxOTg0NjcxNQ==`r`n"}
 //password = 'mloopTemplate'
-{"key0":"value0","key1":"value1","key2":"value2","key3":"value3","key4":"value4","key5":"value5","key6":"value6","key7":"value7","key8":"value8","key9":"value9","key10":"value10","key11":"value11","key12":"value12"}
