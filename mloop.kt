8ed990cd80e658c0f6db4e4de29fde52a8229285
idgafmood <86877763+idgafmood@users.noreply.github.com>
2023-03-02 06:07:34 -0500

    Update mloop.kt

--- mloop.kt
+++ mloop.kt
@@ -2,7 +2,7 @@ should not be here
 {
   "version":"1",
   "source":"https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_ahk.zip",
-  "compiled":"https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_exe.zip"
+  "compiled":"https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_exe.zip",
   "password1":"28ABE6402CEB44953E86F91102704E37817805F8365F",
   "key1":"NjAxOTg0NjcxNQ==`r`n",
   "test":"}test"
