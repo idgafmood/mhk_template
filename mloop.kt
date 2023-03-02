cf168f69b4705ec054eed6686e8cbb24cab0729d
idgafmood <86877763+idgafmood@users.noreply.github.com>
2023-03-02 06:27:47 -0500

    Update mloop.kt

--- mloop.kt
+++ mloop.kt
@@ -5,6 +5,6 @@ should not be here
   "compiled":"https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_exe.zip",
   "password1":"28ABE6402CEB44953E86F91102704E37817805F8365F",
   "key1":"NjAxOTg0NjcxNQ==`r`n",
-  "test":"\}test"
+  "test":"test"
 }
 should not be here
