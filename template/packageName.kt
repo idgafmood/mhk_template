a56980d321b765f00090bd40c3b3366758f3fef8
idgafmood <86877763+idgafmood@users.noreply.github.com>
2023-03-03 06:47:07 -0500

    Update packageName.kt

--- template/packageName.kt
+++ template/packageName.kt
@@ -5,4 +5,5 @@
   "password1":"682B729747BC1F3C2B2659461B57",
   "key1":"a2V5`r`n"
 }
-//this is a template for '_.urlLoad()'//
+//this is a template for '_.urlLoad()' get each line with: '_.s_[number]', or get json portion with: '_.json2object(_.s_)'//
+//try to only include extra information below here//
