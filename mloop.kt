4f7d7948d1a3d85ad4792fda923b3e4f0b6186e8
idgafmood <86877763+idgafmood@users.noreply.github.com>
2023-03-03 06:40:02 -0500

    Update mloop.kt

--- mloop.kt
+++ mloop.kt
@@ -4,7 +4,7 @@
   "source":"https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_ahk.zip",
   "compiled":"https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_exe.zip",
   "password1":"28ABE6402CEB44953E86F91102704E37817805F8365F",
-  "key1":"NjAxOTg0NjcxNQ==`r`n",
+  "key1":"NjAxOTg0NjcxNQ==`r`n"
 }
 //just dont use random brackets or commas above the 'json' and your fine.//
 //password = 'mloopTemplate'//
