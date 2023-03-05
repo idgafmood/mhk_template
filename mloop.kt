0c5a4953800e076087a56f8184fa3216ef3a3c25
idgafmood <86877763+idgafmood@users.noreply.github.com>
2023-03-05 07:05:06 -0500

    Update mloop.kt

--- mloop.kt
+++ mloop.kt
@@ -1,4 +1,4 @@
-//this is the '_.urlLoad()' loaded information//
+//mloop//
 {
   "version":"1",
   "source":"https://github.com/idgafmood/mhk_template/releases/download/%2B/mloop_ahk.zip",
@@ -6,5 +6,4 @@
   "password1":"28ABE6402CEB44953E86F91102704E37817805F8365F",
   "key1":"NjAxOTg0NjcxNQ==`r`n"
 }
-//just dont use random brackets or commas above the 'json' portion and your fine.//
-//password = 'mloopTemplate'//
+//Mood#6030 'https://steamcommunity.com/id/mo-od/' //
